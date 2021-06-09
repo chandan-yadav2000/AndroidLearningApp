c18abdbbf3c05f93bae07a52a81201f25c9a61cb
Mr-cha <chandany535@gmail.com>
2021-06-09 15:38:21 +0530

    Splash

--- app/src/main/java/com/example/learningapp/MainActivity.java
+++ app/src/main/java/com/example/learningapp/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread(){
+            public void  run(){
+          try{
+              sleep(5*1000);
+              Intent intent = new Intent(MainActivity.this,DashboardActivity.class);
+              startActivity(intent);
+              finish();
+          }catch (Exception e){
+              Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+          }
+            }
+        };
+        thread.start();
     }
 }
