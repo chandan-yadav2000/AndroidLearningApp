7c1d4dfb3cd5d87fa155d11d45b38af88446a1dc
Mr-cha <chandany535@gmail.com>
2021-06-17 13:13:46 +0530

    Internet Connection

--- app/src/main/java/com/example/learningapp/MainActivity.java
+++ app/src/main/java/com/example/learningapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.learningapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.TextureView;
 import android.view.View;
@@ -27,30 +31,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.label1);
-        AlphaAnimation alphaAnimation = new AlphaAnimation(0.0f,1.0f);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0.0f, 1.0f);
         alphaAnimation.setDuration(2000);
         textView.startAnimation(alphaAnimation);
 
         ImageView imageView = findViewById(R.id.logo);
-        RotateAnimation rotateAnimation = new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF,0.5f, Animation.RELATIVE_TO_SELF,0.5f);
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         rotateAnimation.setDuration(2000);
         rotateAnimation.setInterpolator(new LinearInterpolator());
         imageView.startAnimation(rotateAnimation);
 
-
-        Thread thread = new Thread(){
-            public void  run(){
-          try{
-              sleep(5*1000);
-              Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-              startActivity(intent);
-              finish();
-          }catch (Exception e){
-              Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
-          }
-            }
-        };
-        thread.start();
+        if (!isConnected(MainActivity.this)) {
+            builderDialog(MainActivity.this).show();
+        } else {
+            Thread thread = new Thread() {
+                public void run() {
+                    try {
+                        sleep(5 * 1000);
+                        Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } catch (Exception e) {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            };
+            thread.start();
+        }
     }
+    public boolean isConnected(Context c){
+        ConnectivityManager cm = (ConnectivityManager) c.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        if(networkInfo != null && networkInfo.isConnectedOrConnecting()){
+            android.net.NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            android.net.NetworkInfo mobile = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+            return mobile != null && networkInfo.isConnectedOrConnecting() ||
+                (wifi != null && networkInfo.isConnectedOrConnecting());
+        }else return false;
+    }
+
+public AlertDialog.Builder builderDialog(Context c){
+        AlertDialog.Builder alert = new AlertDialog.Builder(c);
+        alert.setTitle("Network Issue");
+        alert.setMessage("Internet is not available please cross check your connection....");
+        alert.setIcon(R.drawable.alerticon);
+        alert.setPositiveButton("OK",(dialog, which) -> finish());
+        return alert;
+}
 
 }
